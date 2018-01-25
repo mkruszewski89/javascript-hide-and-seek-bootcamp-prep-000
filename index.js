1be30dad6b7dfbdcaa5d5d08c10e4113f3065696
Michael Kruszewski <mdkruszewski@gmail.com>
2018-01-25 02:05:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ function getFirstSelector(selector) {
 }
 
 function nestedTarget() {
-  return document.getElementById('nested').getElementsByClassName('target')[0]
+  return document.getElementById('nested').getElementsByClassName('target')
 }
