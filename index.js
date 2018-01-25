34c1d0e135ba4ec755670d483cdba619cf6bcfc4
Michael Kruszewski <mdkruszewski@gmail.com>
2018-01-25 01:49:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ function getFirstSelector(selector) {
 
 function nestedTarget() {
   //return document.querySelectorAll(document.getElementById('nested'))
-  return document.querySelector(document.getElementById('nested').target)
+  return document.querySelectorAll(document.getElementById('nested'))
 }
